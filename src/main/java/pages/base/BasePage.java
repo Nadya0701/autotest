dea13abab72f755d485c2bfa6c238bf4999d55d0
Nadya0701 <127718321+Nadya0701@users.noreply.github.com>
2023-10-30 17:02:35 +0400

    Remove extra imports in BasePage

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,14 +1,12 @@
 package pages.base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Set;
+
 
 import static constants.Constant.TimeoutVariable.EXPLICIT_WAIT;
 
